e1aa3b63ddb5ee05711507732c3132ae4086305d
MinaBurns <36201813+MinaBurns@users.noreply.github.com>
2018-02-21 23:23:50 -0800

    Update Lab11j.java

--- Unit 7/src/Lab11j.java
+++ Unit 7/src/Lab11j.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -10,6 +10,11 @@ public class Lab11j
 {
    public static void main(String args[])
    {
-
+		Scanner keyboard = new Scanner(System.in);
+		//char response;
+		out.print("Enter the upper limit");
+		int limit = keyboard.nextInt();
+		Triples one = new Triples(limit);
+		one.toString();
 	}
 }
