42b203f48023a8a1dbea671f0ad7d513b096e033
MinaBurns <36201813+MinaBurns@users.noreply.github.com>
2018-02-26 09:30:33 -0800

    Update RomanNumeral.java

--- Unit 8/src/RomanNumeral.java
+++ Unit 8/src/RomanNumeral.java
@@ -33,38 +33,94 @@ public class RomanNumeral
 
 	public void setNumber(Integer num)
 	{
-		for (int i = 0; i < NUMBERS.Length; i++)
-		{
-			if (num >= NUMBERS[i])
-			{
-				roman = roman + LETTERS[i];
-				num = num - NUMBERS[i];
-			}
-		}
+		number = num;
+		//for (int i = 0; i < NUMBERS.Length; i++)
+		//{
+			//if (num >= NUMBERS[i])
+			//{
+				//roman = roman + LETTERS[i];
+				//num = num - NUMBERS[i];
+			//}
+		//}
 	}
 
 	public void setRoman(String rom)
 	{
-		for (int i = 0; i < rom.length; i++)
-		{
-			if (rom.charAt(i) = LETTERS[i])
-			{
-				number = number + NUMBERS[i];
-				else if (rom.substring(i, i+1) = LETTERS[i])
-				{
-					number = number + NUMBERS[i];
-				}
-			}
-		}
+		roman = rom;
+		//for (int i = 0; i < rom.length; i++)
+		//{
+			//if (rom.charAt(i) = LETTERS[i])
+			//{
+				//number = number + NUMBERS[i];
+				//else if (rom.substring(i, i+1) = LETTERS[i])
+				//{
+					//number = number + NUMBERS[i];
+				//}
+			//}
+		//}
 	}
 
 	public Integer getNumber()
 	{
-		return number;
+		String modified = roman;
+		String firstTwo = modified.substring(0,2);
+		String first = modified.substring(0,1);
+		int total = 0;
+		for (int i = 0; i<LETTERS.length; i++)
+		{
+			if (firstTwo.equals(LETTERS[i]))
+			{
+				modified = modified.substring(2);
+				total +=NUMBERS[i];
+				i=-1;
+				if(modified.length()>=2)
+					firstTwo = modified.substring(0,2);
+				if (modified.length()>=1)
+					first = modified.substring(0,1);
+			}
+			else if (first.equals(LETTERS[i]))
+			{
+				modified = modified.substring(1);
+				total+= NUMBERS[i];
+				i=-1;
+				if(modified.length()>=2)
+					firstTwo = modified.substring(0,2);
+				if (modified.length()>=1)
+					first = modified.substring(0,1);
+			}
+			else if (modified.isEmpty() == true)
+			{
+				return total;
+			}
+		}
+			return number;
 	}
 
 	public String toString()
 	{
+		int modified = number;
+		String output = "";
+		for (int i = 0; i <NUMBERS.length; i++)
+		{
+			if (number == NUMBERS[i])
+			{
+				return LETTERS[i];
+			}
+			else if ((modified-NUMBERS[i]) > 0)
+			{
+				for (int ii = 0; ii<NUMBERS.length; ii++)
+				{
+					if ((modified-NUMBERS[ii]) >= 0)
+					{
+						modified = (modified-NUMBERS[ii]);
+						output +=LETTERS[ii];
+						ii=-1;
+					}
+					else if (modified ==0)
+						return output;
+				}
+			}
+		}
 		return roman + "\n";
 	}
 }
